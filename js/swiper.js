971e22880ffce9733471e34ef66b70dcd732b064
fathima-nihala <137775396+fathima-nihala@users.noreply.github.com>
2024-07-22 10:54:14 +0530

    init

--- js/swiper.js
+++ js/swiper.js
@@ -11,7 +11,21 @@ var swiper = new Swiper('.swiper-container', {
         prevEl: '.swiper-button-prev',
     },
     autoplay: {
-delay: 2500,
-disableOnInteraction: false,
-},
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        768:{
+            slidesPerView: 3.5,
+            // slidesPerGroup: 2,
+        },
+        400: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+        },
+        320:{
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+        }
+    }
 });
