32d5d4cd8a7f961a74469af5169e3232cfc2d05a
fathima-nihala <137775396+fathima-nihala@users.noreply.github.com>
2024-08-05 23:44:35 +0530

    init

---
+++ Api/category.js
@@ -0,0 +1,81 @@
+    // document.addEventListener('DOMContentLoaded', function() {
+    //     fetch('http://localhost:7006/api/category')
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             console.log(data,'ffff');
+    //             if (data.success) {
+    //                 const carousel = document.getElementById('categoryCarousel');
+    //                 data.category.forEach(category => {
+    //                     const categoryHtml = `
+    //                         <div>
+    //                             <a href="single-property.html" class="pxp-prop-card-1 rounded-lg">
+    //                                 <div class="pxp-prop-card-1-fig pxp-cover"
+    //                                     style="background-image: url(${category.image});"></div>
+    //                                 <div class="pxp-prop-card-1-gradient pxp-animate"></div>
+    //                                 <div class="pxp-prop-card-1-details">
+    //                                     <div class="pxp-prop-card-1-details-price">${category.name}</div>
+    //                                 </div>
+    //                                 <div class="pxp-prop-card-1-details-cta text-uppercase">View Details</div>
+    //                             </a>
+    //                         </div>
+    //                     `;
+    //                     carousel.innerHTML += categoryHtml;
+    //                 });
+
+    //                 $('.owl-carousel').owlCarousel({
+    //                 });
+    //             } else {
+    //                 console.error('Failed to fetch categories');
+    //             }
+    //         })
+    //         .catch(error => console.error('Error:', error));
+    // });
+
+
+
+
+    document.addEventListener('DOMContentLoaded', function() {
+            fetch('http://localhost:7006/api/category')
+                .then(response => response.json())
+                .then(data => {
+                    if (data.success) {
+                        const carousel = document.getElementById('categoryCarousel');
+                        data.category.forEach(category => {
+                            const categoryHtml = `
+                                <div>
+                                    <a href="single-property.html" class="pxp-prop-card-1 rounded-lg" >
+                                        <div class="pxp-prop-card-1-fig pxp-cover"
+                                            style="background-image: url(${category.image});"></div>
+                                        <div class="pxp-prop-card-1-gradient pxp-animate"></div>
+                                        <div class="pxp-prop-card-1-details">
+                                            <div class="pxp-prop-card-1-details-price" ">${category.name}</div>
+                                        </div>
+                                        <div class="pxp-prop-card-1-details-cta text-uppercase">View Details</div>
+                                    </a>
+                                </div>
+                            `;
+                            carousel.innerHTML += categoryHtml;
+                        });
+
+                        $('.owl-carousel').owlCarousel({
+                            // loop: true,
+                            // margin: 10,
+                            // nav: true,
+                            // responsive: {
+                            //     0: {
+                            //         items: 1
+                            //     },
+                            //     600: {
+                            //         items: 3
+                            //     },
+                            //     1000: {
+                            //         items: 5
+                            //     },
+                            // },
+                        });
+                    } else {
+                        console.error('Failed to fetch categories');
+                    }
+                })
+                .catch(error => console.error('Error:', error));
+        });
